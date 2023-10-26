de813a75997a3e8d51c43dc61b7116c047b7f3c8
Daniya Yelemessova <daniya@collingsmail.com>
2023-10-26 14:03:21 -0400

    Add additional functions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 function add(number1, number2){
-  return number1 + number2
-}
+  return  number1 + number2
+  }
+
+function sub(number1, number2){
+  return number1 - number2
+  }
+
+  function multiply(number1, number2){
+    return number1 * number2
+    }
 
 const number1 = parseInt(prompt("Enter a number"));
 const number2 = parseInt(prompt("Enter another number"));
 
-window.alert(add(number1, number2))
+
+window.alert(number1 + " - " + number2 + " = " + sub(number1, number2) + ".\n" +
+number1 + "+" + number2 + "=" + add(number1,number2));
